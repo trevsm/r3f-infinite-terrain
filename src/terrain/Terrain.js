1fc43ec04184cb7c9b05e7ec79cc2d3a52527ad4
trevsm <trevsm@pm.me>
2020-12-27 22:10:28 -0700

    Added Perlin Noise

--- src/terrain/Terrain.js
+++ src/terrain/Terrain.js
@@ -2,18 +2,14 @@ import React from 'react'
 import * as THREE from 'three'
 import { useControl } from 'react-three-gui'
 
-import { math } from '../functions/Math'
+import { perlin } from '../functions/Math'
 
 function _ModifyVertices(geometry, size, a, b) {
   geometry.vertices.forEach(v => {
-    let center = new THREE.Vector2(0, 0)
-    let vertex = new THREE.Vector2(v.x, v.y)
-
-    // let dist = vertex.distanceTo(center)
-    // let h = 1 - math.sat(dist / size)
-    // h = h * h + h * (h * (h * a + b) + c)
-
-    v.z = a * Math.sin(b * v.x) + a * Math.cos(b * v.y)
+    let height = a * perlin.get(b * v.x, b * v.y)
+    height = height < 0.5 ? 1 : 0
+    height = height < 0.5 ? 1 : 0
+    v.z = height
   })
 }
 
@@ -21,13 +17,24 @@ const Plane = props => {
   const S = useControl('Size', { type: 'number', value: 15, min: 10, max: 30 })
   const V = useControl('Vertices', {
     type: 'number',
-    value: 50,
+    value: 70,
     min: 10,
     max: 100,
   })
 
-  const a = useControl('A', { type: 'number', value: .4, min: -Math.PI, max: Math.PI })
-  const b = useControl('B', { type: 'number', value: 2.2, min: -Math.PI, max: Math.PI })
+  const a = useControl('Amplitude', {
+    type: 'number',
+    value: 1.5,
+    min: -Math.PI,
+    max: Math.PI,
+  })
+
+  const b = useControl('Length', {
+    type: 'number',
+    value: 0.5,
+    min: -Math.PI,
+    max: Math.PI,
+  })
 
   const geometry = new THREE.PlaneGeometry(S, S, V, V)
 

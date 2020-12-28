694ca110178b2b0faac349d90db5f3cfdec36ffe
trevsm <trevsm@pm.me>
2020-12-27 18:53:04 -0700

    Added sin height filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { Canvas } from 'react-three-fiber'
+import { Controls } from 'react-three-gui';
 import { OrbitControls, softShadows } from '@react-three/drei'
 
 import { Lighting } from './lighting/Lighting'
@@ -12,14 +12,17 @@ softShadows()
 
 export default function App() {
   return (
-    <Canvas
+    <Controls.Provider
       shadowMap
       gl={{ alpha: false }}
-      camera={{ position: [-10, 3, 10], fov: 60 }}
+      camera={{ position: [20, 3, 20], fov: 60 }}
     >
-      <OrbitControls />
-      <Lighting />
-      <Terrain />
-    </Canvas>
+      <Controls.Canvas>
+        <OrbitControls />
+        <Lighting />
+        <Terrain />
+      </Controls.Canvas>
+      <Controls />
+    </Controls.Provider>
   )
 }
--- src/terrain/Terrain.js
+++ src/terrain/Terrain.js
@@ -1,25 +1,45 @@
 import React from 'react'
 import * as THREE from 'three'
+import { useControl } from 'react-three-gui'
 
-function _ModifyVertices(geometry) {
-  const amplitude = 1
+import { math } from '../functions/Math'
+
+function _ModifyVertices(geometry, size, a, b) {
   geometry.vertices.forEach(v => {
-    v.z = Math.random() * amplitude
+    let center = new THREE.Vector2(0, 0)
+    let vertex = new THREE.Vector2(v.x, v.y)
+
+    // let dist = vertex.distanceTo(center)
+    // let h = 1 - math.sat(dist / size)
+    // h = h * h + h * (h * (h * a + b) + c)
+
+    v.z = a * Math.sin(b * v.x) + a * Math.cos(b * v.y)
   })
 }
 
 const Plane = props => {
-  const geometry = new THREE.PlaneGeometry(10, 10, 20, 20)
+  const S = useControl('Size', { type: 'number', value: 15, min: 10, max: 30 })
+  const V = useControl('Vertices', {
+    type: 'number',
+    value: 50,
+    min: 10,
+    max: 100,
+  })
 
-  _ModifyVertices(geometry)
+  const a = useControl('A', { type: 'number', value: .4, min: -Math.PI, max: Math.PI })
+  const b = useControl('B', { type: 'number', value: 2.2, min: -Math.PI, max: Math.PI })
+
+  const geometry = new THREE.PlaneGeometry(S, S, V, V)
+
+  _ModifyVertices(geometry, S, a, b)
 
   const material = new THREE.MeshBasicMaterial({
     color: 'grey',
     side: THREE.DoubleSide,
-    wireframe:true
+    wireframe: true,
   })
 
-  return <mesh args={[geometry, material]} {...props}/>
+  return <mesh args={[geometry, material]} {...props} />
 }
 
 export const Terrain = () => {
